9ddd446c1d6e1132c126357cd91f4d537780e701
Bernardo Gonçalves Spinelli Martins <107887848+Spinelli666@users.noreply.github.com>
2024-05-22 17:08:37 -0300

    Aula 9

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,23 +1,15 @@
 import './App.css';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
 
-  const nome = "Maria";
-
   return (
     <div className="App">
-      <h1>Testando CSS</h1>
-      <Frase />
-      <Frase />
-      <SayMyName nome="Bernardo" />
-      <SayMyName nome="João" />
-      <SayMyName nome={nome} />
-      <Pessoa nome="Bernardo" idade="22" profissao="Estagiário" foto="https://via.placeholder.com/150" />
-      <List />
+      <h1>Testando Eventos</h1>
+      <Evento numero="1" />
+      <Evento numero="2" />
+      <Form />
     </div>
   );
 }
